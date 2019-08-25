4eb241a7176ccf919d055bd5257cd8af21c70ad7
MlleMillzy <54508807+MlleMillzy@users.noreply.github.com>
2019-08-25 21:13:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,37 @@
-const Discord = require('discord.js');
+client.commands = new Discord.Collection();
+const fs = require('fs');
+const prefix = "/";
 
-const client = new Discord.Client();
 
- 
 
-client.on('ready', () => {
+fs.readdir('./Commandes/', (error, f) => {
+    if (error) { return console.error(error); }
+        let commandes = f.filter(f => f.split('.').pop() === 'js');
+        if (commandes.length <= 0) { return console.log('Aucune commande trouvée !'); }
 
-    console.log('I am ready!');
-
+        commandes.forEach((f) => {
+            let commande = require(`./Commandes/${f}`);
+            console.log(`${f} commande chargée !`);
+            client.commands.set(commande.help.name, commande);
+        });
 });
 
- 
+fs.readdir('./Events/', (error, f) => {
+    if (error) { return console.error(error); }
+        console.log(`${f.length} events chargés`);
 
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+        f.forEach((f) => {
+            let events = require(`./Events/${f}`);
+            let event = f.split('.')[0];
+            client.on(event, events.bind(null, client));
+        });
 });
 
 
 
-client.login(process.env.NjE1MjE5NjE0Nzc0NzIyNTgy.XWK2QA.Tjs2C_kE31KfmNkcpbeMuVd-JX4)
+client.on('ready',() => {
+    client.user.setActivity("/music", { type: "PLAYING"});
+    console.log("Je suis de nouveau connecté !");
+  });
+  
+  client.login("NjA0NjAxOTI1ODI4MDgzNzEz.XT2SJw.s-LaqyAQrXbWnzz1zeYBltRWT_E");
